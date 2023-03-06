717ac5f3600925c5b9d5e90ff1eb9f9cf9a77254
Michael Gatuma <50529359+michaelgatuma@users.noreply.github.com>
2023-03-06 09:52:07 +0300

    api: update api status response and query params

--- api/app/Http/Resources/UserResource.php
+++ api/app/Http/Resources/UserResource.php
@@ -6,6 +6,7 @@ use App\Services\WeatherService;
 use Cache;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Http;
 
 class UserResource extends JsonResource
 {
